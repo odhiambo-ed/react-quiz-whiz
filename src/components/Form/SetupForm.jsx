f80572cd625a4f5aacff82ac2e0198c497306410
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-29 23:14:01 +0300

    add form

--- src/components/Form/SetupForm.jsx
+++ src/components/Form/SetupForm.jsx
@@ -2,8 +2,35 @@
 
 function SetUpForm() {
   return (
-    <div>SetUpForm</div>
-  )
+    <main>
+      <section className="quiz quiz-small">
+        <form action="">
+          <h2>Choose the type of Quiz Set</h2>
+          <div className="form-control">
+            <label htmlFor="amount">number of questions</label>
+            <input type="number" id="amount" className="form-input" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="category">category</label>
+            <select name="category" id="category" className="form-input">
+              <option value="sports">sports</option>
+              <option value="history">history</option>
+              <option value="politics">politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <label htmlFor="dificulty">dificulty</label>
+            <select name="dificulty" id="dificulty" className="form-input">
+              <option value="ease">ease</option>
+              <option value="medium">medium</option>
+              <option value="hard">hard</option>
+            </select>
+                  </div>
+                  <p className="error"></p>
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default SetUpForm
