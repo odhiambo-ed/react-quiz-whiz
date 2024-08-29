db23caf438c87f62c1f9fd5d053facecfef57e74
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-30 00:00:45 +0300

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
-import SetUpForm from './components/Form/SetUpForm'
-import Loading from './components/Loading/Loading'
-import Modal from './components/Modal/Modal'
+// import SetUpForm from './components/Form/SetUpForm'
+// import Loading from './components/Loading/Loading'
+// import Modal from './components/Modal/Modal'
 
 function App() {
 
   return (
     <div className='main'>
-      <SetUpForm />
+      <section className='quiz'>
+        <p className='correct-answers'>correct answers: 10</p>
+        <article className='container'>
+          <h2>Text</h2>
+          <div className="btn-container"></div>
+        </article>
+        <button className='next-question'>next quiz</button>
+      </section>
     </div>
   )
 }
