4104774c03d3d15a55489a67f2e661d23e80fcc7
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-30 00:21:08 +0300

    add app states

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 
-
-function Context() {
-  return (
-    <div>Context</div>
-  )
-}
-
-export default Context
+const AppProvider = () => {
+    const [error, setError] = useState(false)
+    const [waiting, setWaiting] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [question, setQuestion] = useState([])
+    const [index, setIndex] = useState(0)
+    const [correct, setCorrect] = useState(0)
+    const [quiz, setQuiz] = useState({
+        amount: 10,
+        category: "sports",
+        difficulty: "ease"
+    })
+ }
