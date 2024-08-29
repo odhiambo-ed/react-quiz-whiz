d57b54b9503aa58870af3f48a79fa2982e0f1851
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-29 23:52:27 +0300

    import loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import SetUpForm from './components/Form/SetUpForm'
+import Loading from './components/Loading/Loading'
+import Modal from './components/Modal/Modal'
 
 function App() {
 
