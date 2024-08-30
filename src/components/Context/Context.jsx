2217398c3470201bda51702a7fb9e28b31e60bb5
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-30 16:27:06 +0300

    add table object

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,15 +1,60 @@
-import { useState } from "react"
+import { createContext, useContext, useState } from "react"
 
-const AppProvider = () => {
+const table = {
+    sports: 21,
+    history: 23,
+    politics: 24,
+};
+
+const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+
+const AppContext = createContext()
+const AppProvider = ({ children }) => {
     const [error, setError] = useState(false)
-    const [waiting, setWaiting] = useState(false)
+    const [waiting, setWaiting] = useState(true)
     const [loading, setLoading] = useState(false)
-    const [question, setQuestion] = useState([])
+    const [questions, setQuestions] = useState([])
     const [index, setIndex] = useState(0)
     const [correct, setCorrect] = useState(0)
+    const [modal, setModal] = useState(false)
     const [quiz, setQuiz] = useState({
         amount: 10,
         category: "sports",
         difficulty: "ease"
     })
- }
+
+    const fetchQuestions = async () => {
+        setLoading(true)
+        setWaiting(false)
+        try {
+            const res = await fetch("https://opentdb.com/api.php?amount=10&category=24&difficulty=medium");
+            const result = await res.json()
+            const data = result.data.results
+
+            if (data) {
+                setQuestions(data)
+                setLoading(false)
+                setWaiting(false)
+                setError(false)
+            } else {
+                setLoading(true)
+                setWaiting(true)
+            }
+
+        } catch (e) {
+            setError(e.message)
+        }
+    }
+
+    return (
+        <AppContext.provider>
+            {children}
+        </AppContext.provider>
+    )
+};
+
+export useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export { AppContext, AppProvider };
